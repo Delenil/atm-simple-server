4356f951935d415e686870b6546efcd8ae784fce
Vasko <vaskopoh@gmail.com>
2025-04-13 15:29:25 +0300

    Basic Skeleton for ATMServer

--- src/ATMServer.java
+++ src/ATMServer.java
@@ -1,2 +1,23 @@
+import java.net.*;
+import java.io.*;
+
 public class ATMServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(12345);
+            System.out.println("ATM Server started...");
+
+             while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected");
+                // Code to handle the client will be added
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 12345.");
+            System.exit(1);
+        } finally {
+            serverSocket.close();
+        }
+    }
 }
